7985304350129c0ff3b3a56721b60fae5f5efffa
alexschoefer <alex.schoefer@gmx.net>
2025-04-01 20:53:55 +0200

    first steps

---
+++ assets/scripts/dishes.js
@@ -0,0 +1,169 @@
+let menuDishes = {
+    "appetizers":[
+        {
+            "name": "Bruschetta",
+            "price": 7.90,
+            "description": "Weißbrot mit frischen Tomaten, Knoblauch und Basilikum",
+            "amount": 0
+        },
+        {
+            "name": "Antipasto Italiano",
+            "price": 9.90,
+            "description": "gemischte Vorspeisenplatte",
+            "amount": 0
+        },
+        {
+            "name": "Caprese con Prosciutto di Parmma",
+            "price": 11.50,
+            "description": "mit Tomaten, Mozzarella und Parmaschinken",
+            "amount": 0
+        },
+        {
+            "name": "Carpaccio di Manzo",
+            "price": 10.50,
+            "description": "Rindfleisch-Carpaccio mit Rucola und Parmesan",
+            "amount": 0
+        },
+    ],
+    "salads": [
+        {
+            "name": "Insalata Mista",
+            "price": 6.90,
+            "description": "gemischter Salat",
+            "amount": 0
+        },
+        {
+            "name": "Insalata Tonno",
+            "price": 10.50,
+            "description": "gemischter Salat mit Thunfisch, Mais, Ei und Zwiebeln",
+            "amount": 0
+        },
+        {
+            "name": "Insalata Con Pollo",
+            "price": 11.50,
+            "description": "gemischter Salat mit Hähnchenbrust",
+            "amount": 0
+        },
+        {
+            "name": "Insalata Con Gamberi",
+            "price": 11.50,
+            "description": "gemischter Salat mit Garnelen",
+            "amount": 0
+        }
+    ],
+    "wraps":[],
+    "pizza":[
+        {
+            "name": "Pizza Funghi",
+            "price": 10.90,
+            "description": "mit Champignons",
+            "amount": 0
+        },
+        {
+            "name": "Pizza Salame",
+            "price": 10.90,
+            "description": "mit Salami",
+            "amount": 0
+        },
+        {
+            "name": "Pizza Divalo",
+            "price": 11.80,
+            "description": "Tomatensauce, Käse und Salami und Jalapenos ( Scharf! )",
+            "amount": 0
+        },
+        {
+            "name": "Pizza Vulcano",
+            "price": 13.50,
+            "description": "Tomatensauce, Käse, Schinken, Paprika, Champignons und Spiegelei",
+            "amount": 0
+        }
+    ],
+    "pasta":[
+        {
+            "name": "Spaghetti Napoli",
+            "price": 9.20,
+            "description": "mit Tomatensauce",
+            "amount": 0
+        },
+        {
+            "name": "Spaghetti Bolognese",
+            "price": 10.90,
+            "description": "mit Hackfleisch und Tomatensauce",
+            "amount": 0
+        },
+        {
+            "name": "Pasta Arrabbiata (scharf)",
+            "price": 11.80,
+            "description": "mit scharfer Tomatensauce",
+            "amount": 0
+        },
+        {
+            "name": "Pasta Verdura",
+            "price": 11.90,
+            "description": "mit frischem Gemüse und Sauce nach Wahl",
+            "amount": 0
+        }
+    ],
+    "burger": [
+        {
+            "name": "Cheeseburger",
+            "price": 12.90,
+            "description": "mit Schmelzkäse überbacken",
+            "amount": 0
+        },
+        {
+            "name": "Bacon Burger",
+            "price": 13.90,
+            "description": "mit Schmelzkäse überbacken und knusprigem Speck, dazu Country Potatoes und feiner Dip",
+            "amount": 0
+        },
+        {
+            "name": "Allershausen Burger",
+            "price": 14.90,
+            "description": "mit karamellisierten Zwiebeln, frischem Salat, Schmelzkäse, Spiegelei und knusprigem Speck",
+            "amount": 0
+        },
+        {
+            "name": "The Chicken Dzlan Cheeseburger",
+            "price": 12.90,
+            "description": "mit Schmelzkäse überbacken, dazu Country Potatoes und feiner Dip",
+            "amount": 0
+        }
+    ],
+    "desserts": [
+        {
+            "name": "Warmes Schokotörtchen",
+            "price": 6.50,
+            "description": "mit Vanilleeis und Sahne",
+            "amount": 0
+        },
+        {
+            "name": "Eis & Heiß",
+            "price": 6.90,
+            "description": "Vanilleeis mit heißen Himbeeren und Sahne",
+            "amount": 0
+        },
+        {
+            "name": "Coup Dänemark",
+            "price": 6.90,
+            "description": "Vanilleeis mit Schokosauce und Sahne",
+            "amount": 0
+        },
+        {
+            "name": "Almondy Mandeltorte",
+            "price": 4.90,
+            "description": "mit Karamell und Erdnüssen",
+            "amount": 0
+        }
+    ]
+}
+
+let dishCategories = {
+    "appetizers": "Vorspeisen",
+    "salads": "Salate",
+    "wraps": "Wraps",
+    "pizza": "Pizza",
+    "pasta": "Pasta",
+    "burger": "Burger",
+    "desserts": "Dessert"
+}
