419916426f89070cbc22e68b7ec82cb19aadbdbb
alexschoefer <alex.schoefer@gmx.net>
2025-04-05 21:38:09 +0200

    Erweiterung BestellApp: Dish-Infos, Button

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function init() {
     renderAppetizers();
+    renderSalads();
+    renderPizza();
+    renderPasta();
+    renderBurger();
+    renderDesserts();
 }
 
 function renderAppetizers() {
@@ -7,6 +12,51 @@ function renderAppetizers() {
     appetizersRef.innerHTML="";
 
     for (let indexDishes = 0; indexDishes < menuDishes.appetizers.length; indexDishes++) {
-        appetizersRef.innerHTML += getDishesTemplate(indexDishes);
+        appetizersRef.innerHTML += getDishesAppetizersTemplate(indexDishes);
+    }
+}
+
+function renderSalads() {
+    let saladsRef = document.getElementById('salads-dishes');
+    saladsRef.innerHTML="";
+
+    for (let indexDishes = 0; indexDishes < menuDishes.appetizers.length; indexDishes++) {
+        saladsRef.innerHTML += getDishesSaladsTemplate(indexDishes);
+    }
+}
+
+function renderPizza() {
+    let pizzaRef = document.getElementById('pizza-dishes');
+    pizzaRef.innerHTML="";
+
+    for (let indexDishes = 0; indexDishes < menuDishes.pizza.length; indexDishes++) {
+        pizzaRef.innerHTML += getDishesPizzaTemplate(indexDishes);
+    }
+}
+
+function renderPasta() {
+    let pastaRef = document.getElementById('pasta-dishes');
+    pastaRef.innerHTML="";
+
+    for (let indexDishes = 0; indexDishes < menuDishes.pasta.length; indexDishes++) {
+        pastaRef.innerHTML += getDishesPastaTemplate(indexDishes);
+    }
+}
+
+function renderBurger() {
+    let burgerRef = document.getElementById('burger-dishes');
+    burgerRef.innerHTML="";
+
+    for (let indexDishes = 0; indexDishes < menuDishes.burger.length; indexDishes++) {
+        burgerRef.innerHTML += getDishesBurgerTemplate(indexDishes);
+    }
+}
+
+function renderDesserts() {
+    let dessertsRef = document.getElementById('dessert-dishes');
+    dessertsRef.innerHTML="";
+
+    for (let indexDishes = 0; indexDishes < menuDishes.desserts.length; indexDishes++) {
+        dessertsRef.innerHTML += getDishesDessertsTemplate(indexDishes);
     }
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,8 +1,77 @@
-function getDishesTemplate(indexDishes) {
+function getDishesAppetizersTemplate(indexDishes) {
     return `
-        <div>
+    <div class="single-dish-selection space-between">
+        <div class="dish-information">
             <h3>${menuDishes.appetizers[indexDishes].name}</h3>
             <p>${menuDishes.appetizers[indexDishes].description}</p>
-            </div>
+            <div class="dish-price">${menuDishes.appetizers[indexDishes].price.toFixed(2).replace(".",",").concat(" €")}</div>
+        </div>
+        <div><img id="add-to-Basket"src="./assets/icons/add-to-basket.png" alt="add-to-basket"></div>
+    </div>    
+        `
+}
+
+function getDishesSaladsTemplate(indexDishes) {
+    return `
+    <div class="single-dish-selection space-between">
+        <div class="dish-information">
+            <h3>${menuDishes.salads[indexDishes].name}</h3>
+            <p>${menuDishes.salads[indexDishes].description}</p>
+            <div class="dish-price">${menuDishes.salads[indexDishes].price.toFixed(2).replace(".",",").concat(" €")}</div>
+        </div>
+        <div><img id="add-to-Basket"src="./assets/icons/add-to-basket.png" alt="add-to-basket"></div>
+    </div>    
+        `
+}
+
+function getDishesPizzaTemplate(indexDishes) {
+    return `
+    <div class="single-dish-selection space-between">
+        <div class="dish-information">
+            <h3>${menuDishes.pizza[indexDishes].name}</h3>
+            <p>${menuDishes.pizza[indexDishes].description}</p>
+            <div class="dish-price">${menuDishes.pizza[indexDishes].price.toFixed(2).replace(".",",").concat(" €")}</div>
+        </div>
+        <div><img id="add-to-Basket"src="./assets/icons/add-to-basket.png" alt="add-to-basket"></div>
+    </div>    
+        `
+}
+
+function getDishesPastaTemplate(indexDishes) {
+    return `
+    <div class="single-dish-selection space-between">
+        <div class="dish-information">
+            <h3>${menuDishes.pasta[indexDishes].name}</h3>
+            <p>${menuDishes.pasta[indexDishes].description}</p>
+            <div class="dish-price">${menuDishes.pasta[indexDishes].price.toFixed(2).replace(".",",").concat(" €")}</div>
+        </div>
+        <div><img id="add-to-Basket"src="./assets/icons/add-to-basket.png" alt="add-to-basket"></div>
+    </div>    
+        `
+}
+
+function getDishesBurgerTemplate(indexDishes) {
+    return `
+    <div class="single-dish-selection space-between">
+        <div class="dish-information">
+            <h3>${menuDishes.burger[indexDishes].name}</h3>
+            <p>${menuDishes.burger[indexDishes].description}</p>
+            <div class="dish-price">${menuDishes.pasta[indexDishes].price.toFixed(2).replace(".",",").concat(" €")}</div>
+        </div>
+        <div><img id="add-to-Basket"src="./assets/icons/add-to-basket.png" alt="add-to-basket"></div>
+    </div>    
+        `
+}
+
+function getDishesDessertsTemplate(indexDishes) {
+    return `
+    <div class="single-dish-selection space-between">
+        <div class="dish-information">
+            <h3>${menuDishes.desserts[indexDishes].name}</h3>
+            <p>${menuDishes.desserts[indexDishes].description}</p>
+            <div class="dish-price">${menuDishes.desserts[indexDishes].price.toFixed(2).replace(".",",").concat(" €")}</div>
+        </div>
+        <div><img id="add-to-Basket"src="./assets/icons/add-to-basket.png" alt="add-to-basket"></div>
+    </div>    
         `
 }
